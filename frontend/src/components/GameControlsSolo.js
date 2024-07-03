7425550d802a2e26ac4a0c38516625c813731b73
Afif Deshmukh <afifkhanab@gmail.com>
2024-07-03 11:38:28 +0530

    ios ui issue trial

--- frontend/src/components/GameControlsSolo.js
+++ frontend/src/components/GameControlsSolo.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { ChooseAnswer } from './ChooseAnswer'
 import { Button } from './ui/button'
 import { useDispatch, useSelector } from 'react-redux'
